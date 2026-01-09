a435628c536bc5b3e09e10222e1aeff6597ce79d
Bruno Odin Valle Mendez <240846@utxicotepec.edu.mx>
2026-01-09 10:08:05 -0600

    Crear funciones y estilizado

--- JS/batman1.js
+++ JS/batman1.js
@@ -1,3 +1,29 @@
 function saludar(){
     alert("Bienvenido a la Academia de T.I");
 }
+function suma(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 + valor2;
+    alert("El resultado de la suma de "+valor1+" y "+valor2+" es:"+resultado)
+}
+function resta(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 - valor2;
+    alert("El resultado de la resta de "+valor1+" y "+valor2+" es:"+resultado)
+}
+
+function multiplicar(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 * valor2;
+    alert("El resultado de la multiplicacion de "+valor1+" y "+valor2+" es:"+resultado)
+}
+
+function dividir(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 / valor2;
+    alert("El resultado de la division de "+valor1+" y "+valor2+" es:"+resultado)
+}
